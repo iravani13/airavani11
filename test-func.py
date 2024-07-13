23a0a32cea389d64e843164bc6ea9bf9e151cf48
amir iravani <airavani1@myseneca.ca>
2024-07-13 14:41:49 -0400

    Removed unnecessary files

--- test-func.py
+++
@@ -1,5 +0,0 @@
-from func import items_after_four
-
-print(items_after_four([1, 2, 3]))       # Output: 0
-print(items_after_four([1, 2, 3, 4, 5])) # Output: 1
-print(items_after_four([1, 2, 3, 4, 5, 6])) # Output: 2
